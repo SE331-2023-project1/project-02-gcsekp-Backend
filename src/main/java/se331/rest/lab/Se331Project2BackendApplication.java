76697ed3284ad1799d077fb1ae2108de20cdd628
Saw <ehkalanephaung96@gmail.com>
2023-10-22 14:13:46 +0700

    setup project

---
+++ src/main/java/se331/rest/lab/Se331Project2BackendApplication.java
@@ -0,0 +1,13 @@
+package se331.rest.lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Se331Project2BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Se331Project2BackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/se331/rest/lab/Se331Project2BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package se331.rest.lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Se331Project2BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
