a1ab345373d6ca3796ddc8c9019f55a4d2db88ad
Saw <ehkalanephaung96@gmail.com>
2023-10-22 22:09:59 +0700

    update data both students and advisor

--- src/main/java/se331/rest/lab/config/InitApp.java
+++ src/main/java/se331/rest/lab/config/InitApp.java
@@ -1,5 +1,7 @@
 package se331.rest.lab.config;
 
+import java.util.List;
+
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
@@ -27,6 +29,8 @@ public class InitApp {
                 .department("English")
                 .position("Lecturer")
                 .advisorID("A001")
+                .image(List.of(
+                        "https://images.pexels.com/photos/2379004/pexels-photo-2379004.jpeg?auto=compress&cs=tinysrgb&w=600"))
                 .build());
         advisor2 = advisorRepository.save(Advisor.builder()
                 .name("Ahmed")
@@ -34,6 +38,8 @@ public class InitApp {
                 .department("Business Administration")
                 .position("Researcher")
                 .advisorID("A002")
+                .image(List.of(
+                        "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=600"))
                 .build());
         advisor3 = advisorRepository.save(Advisor.builder()
                 .name("Astor   ")
@@ -41,6 +47,8 @@ public class InitApp {
                 .department("Mathematics")
                 .position("Professor")
                 .advisorID("A003")
+                .image(List.of(
+                        "https://images.pexels.com/photos/2182970/pexels-photo-2182970.jpeg?auto=compress&cs=tinysrgb&w=600"))
                 .build());
 
         advisor4 = advisorRepository.save(Advisor.builder()
@@ -49,6 +57,8 @@ public class InitApp {
                 .department("History")
                 .position("Associate Professor")
                 .advisorID("A004")
+                .image(List.of(
+                        "https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&w=600"))
                 .build());
 
         advisor5 = advisorRepository.save(Advisor.builder()
@@ -57,6 +67,8 @@ public class InitApp {
                 .department("Physics")
                 .position("Assistant Professor")
                 .advisorID("A005")
+                .image(List.of(
+                        "https://images.pexels.com/photos/18666051/pexels-photo-18666051.jpeg?auto=compress&cs=tinysrgb&w=600"))
                 .build());
         advisor6 = advisorRepository.save(Advisor.builder()
                 .name("David")
@@ -64,6 +76,8 @@ public class InitApp {
                 .department("Computer Science")
                 .position("Professor")
                 .advisorID("A006")
+                .image(List.of(
+                        "https://images.pexels.com/photos/1752126/pexels-photo-1752126.jpeg?auto=compress&cs=tinysrgb&w=600"))
                 .build());
 
         Student student;
@@ -72,6 +86,8 @@ public class InitApp {
                 .name("Daisy")
                 .surname("Walsh")
                 .department("Computer Science")
+                .image(List.of(
+                        "https://images.pexels.com/photos/18678398/pexels-photo-18678398.jpeg?auto=compress&cs=tinysrgb&w=600"))
                 .build());
         student.setAdvisor(advisor1);
         advisor1.getStudentList().add(student);
@@ -81,15 +97,19 @@ public class InitApp {
                 .name("Jerry")
                 .surname("Perry")
                 .department("Mathematics")
+                .image(List.of(
+                        "https://images.pexels.com/photos/997472/pexels-photo-997472.jpeg?auto=compress&cs=tinysrgb&w=600"))
                 .build());
         student.setAdvisor(advisor1);
         advisor1.getStudentList().add(student);
 
         student = studentRepository.save(Student.builder()
                 .studentID("S003")
-                .name("Jerry")
-                .surname("Perry")
+                .name("May")
+                .surname("Gates")
                 .department("Physics")
+                .image(List.of(
+                        "https://images.pexels.com/photos/18618357/pexels-photo-18618357.jpeg?auto=compress&cs=tinysrgb&w=600"))
                 .build());
         student.setAdvisor(advisor2);
         advisor2.getStudentList().add(student);
@@ -99,6 +119,8 @@ public class InitApp {
                 .name("Bob")
                 .surname("Brown")
                 .department("Chemistry")
+                .image(List.of(
+                        "https://images.pexels.com/photos/18619767/pexels-photo-18619767.jpeg?auto=compress&cs=tinysrgb&w=600"))
                 .build());
         student.setAdvisor(advisor2);
         advisor2.getStudentList().add(student);
@@ -108,6 +130,8 @@ public class InitApp {
                 .name("Grace")
                 .surname("Adams")
                 .department("English")
+                .image(List.of(
+                        "https://images.pexels.com/photos/18665105/pexels-photo-18665105.jpeg?auto=compress&cs=tinysrgb&w=600"))
                 .build());
         student.setAdvisor(advisor3);
         advisor3.getStudentList().add(student);
@@ -117,6 +141,8 @@ public class InitApp {
                 .name("Henry")
                 .surname("Wilson")
                 .department("Engineering")
+                .image(List.of(
+                        "https://images.pexels.com/photos/18665111/pexels-photo-18665111.jpeg?auto=compress&cs=tinysrgb&w=600"))
                 .build());
         student.setAdvisor(advisor3);
         advisor3.getStudentList().add(student);
@@ -126,6 +152,8 @@ public class InitApp {
                 .name("Isabella")
                 .surname("Harris")
                 .department("Geology")
+                .image(List.of(
+                        "https://images.pexels.com/photos/18580991/pexels-photo-18580991.jpeg?auto=compress&cs=tinysrgb&w=600"))
                 .build());
         student.setAdvisor(advisor4);
         advisor4.getStudentList().add(student);
@@ -135,6 +163,8 @@ public class InitApp {
                 .name("Jane")
                 .surname("Lee")
                 .department("Psychology")
+                .image(List.of(
+                        "https://images.pexels.com/photos/18641980/pexels-photo-18641980.jpeg?auto=compress&cs=tinysrgb&w=600"))
                 .build());
         student.setAdvisor(advisor4);
         advisor4.getStudentList().add(student);
@@ -144,6 +174,8 @@ public class InitApp {
                 .name("Jackson")
                 .surname("Parker")
                 .department("Computer Science")
+                .image(List.of(
+                        "https://images.pexels.com/photos/18593901/pexels-photo-18593901.jpeg?auto=compress&cs=tinysrgb&w=600"))
                 .build());
         student.setAdvisor(advisor5);
         advisor5.getStudentList().add(student);
@@ -153,6 +185,8 @@ public class InitApp {
                 .name("Sandra")
                 .surname("Bullock")
                 .department("Biology")
+                .image(List.of(
+                        "https://images.pexels.com/photos/18673327/pexels-photo-18673327.jpeg?auto=compress&cs=tinysrgb&w=600"))
                 .build());
         student.setAdvisor(advisor5);
         advisor5.getStudentList().add(student);
@@ -162,6 +196,8 @@ public class InitApp {
                 .name("Scooby")
                 .surname("Doo")
                 .department("Mathematics")
+                .image(List.of(
+                        "https://images.pexels.com/photos/18665098/pexels-photo-18665098.jpeg?auto=compress&cs=tinysrgb&w=600"))
                 .build());
         student.setAdvisor(advisor6);
         advisor6.getStudentList().add(student);
@@ -171,6 +207,8 @@ public class InitApp {
                 .name("Jennifer")
                 .surname("Kim")
                 .department("History")
+                .image(List.of(
+                        "https://images.pexels.com/photos/18633036/pexels-photo-18633036.png?auto=compress&cs=tinysrgb&w=600"))
                 .build());
         student.setAdvisor(advisor6);
         advisor6.getStudentList().add(student);
@@ -180,6 +218,8 @@ public class InitApp {
                 .name("Mika")
                 .surname("Donald")
                 .department("Social")
+                .image(List.of(
+                        "https://images.pexels.com/photos/1486064/pexels-photo-1486064.jpeg?auto=compress&cs=tinysrgb&w=600"))
                 .build());
         student.setAdvisor(advisor1);
         advisor1.getStudentList().add(student);
@@ -189,6 +229,8 @@ public class InitApp {
                 .name("Silver")
                 .surname("Morgan")
                 .department("Geology")
+                .image(List.of(
+                        "https://images.pexels.com/photos/1405963/pexels-photo-1405963.jpeg?auto=compress&cs=tinysrgb&w=600"))
                 .build());
         student.setAdvisor(advisor2);
         advisor2.getStudentList().add(student);
@@ -198,6 +240,8 @@ public class InitApp {
                 .name("Casper")
                 .surname("Miller")
                 .department("Music")
+                .image(List.of(
+                        "https://images.pexels.com/photos/1656684/pexels-photo-1656684.jpeg?auto=compress&cs=tinysrgb&w=600"))
                 .build());
         student.setAdvisor(advisor3);
         advisor3.getStudentList().add(student);
@@ -207,6 +251,8 @@ public class InitApp {
                 .name("Sparkle")
                 .surname("Evans")
                 .department("Biology")
+                .image(List.of(
+                        "https://images.pexels.com/photos/1036645/pexels-photo-1036645.jpeg?auto=compress&cs=tinysrgb&w=600"))
                 .build());
         student.setAdvisor(advisor4);
         advisor4.getStudentList().add(student);
@@ -216,6 +262,8 @@ public class InitApp {
                 .name("Rose")
                 .surname("Lopez")
                 .department("Chemistry")
+                .image(List.of(
+                        "https://images.pexels.com/photos/18667692/pexels-photo-18667692.jpeg?auto=compress&cs=tinysrgb&w=600"))
                 .build());
         student.setAdvisor(advisor5);
         advisor5.getStudentList().add(student);
@@ -225,6 +273,8 @@ public class InitApp {
                 .name("Mickey")
                 .surname("Mouse")
                 .department("Biology")
+                .image(List.of(
+                        "https://images.pexels.com/photos/3214772/pexels-photo-3214772.jpeg?auto=compress&cs=tinysrgb&w=600"))
                 .build());
         student.setAdvisor(advisor6);
         advisor6.getStudentList().add(student);
@@ -234,6 +284,8 @@ public class InitApp {
                 .name("Peter")
                 .surname("Pan")
                 .department("Engineering")
+                .image(List.of(
+                        "https://images.pexels.com/photos/18659930/pexels-photo-18659930.jpeg?auto=compress&cs=tinysrgb&w=600"))
                 .build());
         student.setAdvisor(advisor1);
         advisor1.getStudentList().add(student);
@@ -243,6 +295,8 @@ public class InitApp {
                 .name("Ella")
                 .surname("Robinson")
                 .department("Mathematics")
+                .image(List.of(
+                        "https://images.pexels.com/photos/18663177/pexels-photo-18663177.jpeg?auto=compress&cs=tinysrgb&w=600"))
                 .build());
         student.setAdvisor(advisor2);
         advisor2.getStudentList().add(student);
@@ -252,6 +306,8 @@ public class InitApp {
                 .name("Olaf")
                 .surname("Leonard")
                 .department("Music")
+                .image(List.of(
+                        "https://images.pexels.com/photos/1040881/pexels-photo-1040881.jpeg?auto=compress&cs=tinysrgb&w=600"))
                 .build());
         student.setAdvisor(advisor3);
         advisor3.getStudentList().add(student);
@@ -261,6 +317,8 @@ public class InitApp {
                 .name("Tinkle")
                 .surname("Bell")
                 .department("Physics")
+                .image(List.of(
+                        "https://images.pexels.com/photos/18660742/pexels-photo-18660742.jpeg?auto=compress&cs=tinysrgb&w=600"))
                 .build());
         student.setAdvisor(advisor4);
         advisor4.getStudentList().add(student);
@@ -270,6 +328,8 @@ public class InitApp {
                 .name("Tiger")
                 .surname("Lily")
                 .department("History")
+                .image(List.of(
+                        "https://images.pexels.com/photos/18652727/pexels-photo-18652727.jpeg?auto=compress&cs=tinysrgb&w=600"))
                 .build());
         student.setAdvisor(advisor5);
         advisor5.getStudentList().add(student);
@@ -279,6 +339,8 @@ public class InitApp {
                 .name("Buzz")
                 .surname("Ligthyear")
                 .department("Psychology")
+                .image(List.of(
+                        "https://images.pexels.com/photos/18649280/pexels-photo-18649280.jpeg?auto=compress&cs=tinysrgb&w=600"))
                 .build());
         student.setAdvisor(advisor6);
         advisor6.getStudentList().add(student);
