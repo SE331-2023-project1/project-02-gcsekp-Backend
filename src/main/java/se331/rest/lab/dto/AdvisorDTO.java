404ec8d4461f4543ca22a896f2531288d9ea33fc
Saw <ehkalanephaung96@gmail.com>
2023-10-22 16:54:03 +0700

    Add DTO both Advisor and Student

--- src/main/java/se331/rest/lab/dto/AdvisorDTO.java
+++ src/main/java/se331/rest/lab/dto/AdvisorDTO.java
@@ -1,5 +1,20 @@
 package se331.rest.lab.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class AdvisorDTO {
+    Long id;
+    String studentID;
+    String name;
+    String surname;
+    String image;
+    String department;
 
 }
--- src/main/java/se331/rest/lab/dto/StudentDTO.java
+++ src/main/java/se331/rest/lab/dto/StudentDTO.java
@@ -1,5 +1,21 @@
 package se331.rest.lab.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class StudentDTO {
+    Long id;
+    String name;
+    String surname;
+    String position;
+    String image;
+    String department;
+    StudentDTO student;
 
 }
