b45e3fa18aa007f33a2562557af83dda6f07716b
Saw <ehkalanephaung96@gmail.com>
2023-10-22 17:02:29 +0700

    Add student data

--- src/main/java/se331/rest/lab/config/InitApp.java
+++ src/main/java/se331/rest/lab/config/InitApp.java
@@ -1,31 +1,73 @@
 package se331.rest.lab.config;
 
+import java.util.List;
+
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
 import se331.rest.lab.entity.Advisor;
+import se331.rest.lab.entity.Student;
 import se331.rest.lab.repository.AdvisorRepository;
+import se331.rest.lab.repository.StudentRepository;
 
 @Component
 @RequiredArgsConstructor
 public class InitApp {
     final AdvisorRepository advisorRepository;
+    final StudentRepository studentRepository;
 
     @Override
+    @Transactional
     public void onApplicationEvent(ApplicationReadyEvent applicationReadyEvent) {
-        advisorRepository.save(Advisor.builder()
+        Advisor advisor1, advisor2;
+        Student student1, student2, student3, student4;
+        student1 = studentRepository.save(Student.builder()
+                .studentID("S001")
+                .name("John")
+                .surname("Doe")
+                .department("Computer Science")
+                .build());
+        student2 = studentRepository.save(Student.builder()
+                .studentID("S002")
+                .name("Jane")
+                .surname("Smith")
+                .department("Mathematics")
+                .build());
+        student3 = studentRepository.save(Student.builder()
+                .studentID("S003")
+                .name("Alice")
+                .surname("Johnson")
+                .department("Physics")
+                .build());
+        student4 = studentRepository.save(Student.builder()
+                .studentID("S004")
+                .name("Bob")
+                .surname("Brown")
+                .department("Chemistry")
+                .build());
+        advisor1 = advisorRepository.save(Advisor
+                .builder()
                 .name("Somsak")
                 .surname("Smith")
                 .department("English language teaching")
                 .position("Lecturer")
                 .build());
-        advisorRepository.save(Advisor.builder()
+        advisor1.getStudentList().addAll(List.of(student1, student2));
+        advisor2 = advisorRepository.save(Advisor.builder()
                 .name("Ahmed")
                 .surname("Khan")
                 .department("Business Administration")
                 .position("Researcher")
                 .build());
+        advisor2.getStudentList().addAll(List.of(student3, student4));
+
+        student1.setAdvisor(advisor1);
+        student2.setAdvisor(advisor1);
+        student3.setAdvisor(advisor2);
+        student4.setAdvisor(advisor2);
+
     }
 
 }
---
+++ target/generated-sources/annotations/se331/rest/lab/util/LabMapperImpl.java
@@ -0,0 +1,79 @@
+package se331.rest.lab.util;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.processing.Generated;
+import se331.rest.lab.dto.AdvisorDTO;
+import se331.rest.lab.dto.StudentDTO;
+import se331.rest.lab.entity.Advisor;
+import se331.rest.lab.entity.Student;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2023-10-22T16:59:50+0700",
+    comments = "version: 1.5.5.Final, compiler: Eclipse JDT (IDE) 3.35.0.v20230814-2020, environment: Java 17.0.4.1 (Amazon.com Inc.)"
+)
+public class LabMapperImpl implements LabMapper {
+
+    @Override
+    public StudentDTO getStudentDto(Student student) {
+        if ( student == null ) {
+            return null;
+        }
+
+        StudentDTO.StudentDTOBuilder studentDTO = StudentDTO.builder();
+
+        studentDTO.department( student.getDepartment() );
+        studentDTO.id( student.getId() );
+        studentDTO.image( student.getImage() );
+        studentDTO.name( student.getName() );
+        studentDTO.surname( student.getSurname() );
+
+        return studentDTO.build();
+    }
+
+    @Override
+    public List<StudentDTO> getStudentDto(List<Student> students) {
+        if ( students == null ) {
+            return null;
+        }
+
+        List<StudentDTO> list = new ArrayList<StudentDTO>( students.size() );
+        for ( Student student : students ) {
+            list.add( getStudentDto( student ) );
+        }
+
+        return list;
+    }
+
+    @Override
+    public AdvisorDTO getAdvisorDto(Advisor advisor) {
+        if ( advisor == null ) {
+            return null;
+        }
+
+        AdvisorDTO.AdvisorDTOBuilder advisorDTO = AdvisorDTO.builder();
+
+        advisorDTO.department( advisor.getDepartment() );
+        advisorDTO.id( advisor.getId() );
+        advisorDTO.image( advisor.getImage() );
+        advisorDTO.name( advisor.getName() );
+        advisorDTO.surname( advisor.getSurname() );
+
+        return advisorDTO.build();
+    }
+
+    @Override
+    public List<AdvisorDTO> getAdvisorDto(List<Advisor> advisors) {
+        if ( advisors == null ) {
+            return null;
+        }
+
+        List<AdvisorDTO> list = new ArrayList<AdvisorDTO>( advisors.size() );
+        for ( Advisor advisor : advisors ) {
+            list.add( getAdvisorDto( advisor ) );
+        }
+
+        return list;
+    }
+}
